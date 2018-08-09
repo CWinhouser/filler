d35abb0a2309b4ec608c554ef9f5cc9102b139a9
Kyle TWOMEY <ktwomey@e4r10p5.wethinkcode.co.za>
2018-08-09 14:36:07 +0200

    Planner

--- filler.c
+++ filler.c
@@ -6,13 +6,50 @@
 /*   By: ktwomey <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/09 09:48:31 by ktwomey           #+#    #+#             */
-/*   Updated: 2018/08/09 09:54:34 by ktwomey          ###   ########.fr       */
+/*   Updated: 2018/08/09 14:35:44 by ktwomey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* map size */
-void	ft_size(char *str)
+#include "filler.h"
+
+/* reaad */
+void	ft_read(char **str)
 {
 }
 
+/* Player */
+void	ft_player(char **str)
+{
+}
 
+/* map size */
+void	ft_size(char **str)
+{
+}
+
+/* map */
+void	ft_map(char **str)
+{
+}
+
+/* Piece */
+void	ft_piece(char **str)
+{
+}
+
+/* Position available */
+void	ft_overlap(char **str)
+{
+}
+
+void	ft_pastdot(char **str)
+{
+}
+
+void	ft_finalcheck(char **str)
+{
+}
+
+char	filler(char **str)
+{
+}
