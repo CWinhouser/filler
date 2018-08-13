f8769ae75966ce4bc667248aa2afe5165ff722ed
Kyle TWOMEY <ktwomey@e4r10p5.wethinkcode.co.za>
2018-08-13 14:51:46 +0200

    Player, Size

--- filler.c
+++ filler.c
@@ -6,25 +6,54 @@
 /*   By: ktwomey <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/09 09:48:31 by ktwomey           #+#    #+#             */
-/*   Updated: 2018/08/09 14:35:44 by ktwomey          ###   ########.fr       */
+/*   Updated: 2018/08/13 14:50:39 by ktwomey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-/* reaad */
+/* read */
 void	ft_read(char **str)
 {
+	int		count;
+	char	*line = NULL;
+	char	**map;
+
+	count = 0;
+	map = (char**)malloc(sizeof(map) * (100));
+	while (get_next_line(0, &line)  > 0)
+	{
+		map[count] = ft_strjoin(map[count], line);
+		if (ft_strchr(line, "$$$ exec"))
+			ft_player(line);
+		if (ft_strchr(line, "Plateau"))
+			ft_size(line);
+		count++;
+	}
+	map[count] = NULL;
 }
 
 /* Player */
-void	ft_player(char **str)
+void	ft_player(char *str)
 {
+	t_game		player;
+
+	if(strchr(str, "p1"))
+		player.player = 0;
+	if(strchr(str, "p2"))
+		player.player = 1;
+	player.player = 0;
 }
 
 /* map size */
-void	ft_size(char **str)
+void	ft_size(char *str)
 {
+	t_map		size;
+	char		**store = NULL;
+
+	store = ft_strsplit(str, ' ');
+	size.y_map = itoa(store[1]);
+	size.x_map = itoa(store[2]);
 }
 
 /* map */
